9919c071e724dc9ee787a025a36f19f3103edf5d
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-08-05 23:55:20 +0530

    patches

--- frames-video.py
+++ frames-video.py
@@ -4,28 +4,30 @@ import os
 from os.path import isfile, join
 import datetime as dt
 
-today = str(dt.date.today()).replace('-', '')
+# today = str(dt.date.today()).replace('-', '')
 pathIn= './frames_20190714/'
 pathOut = 'video.mp4'
 fps = 0.035
 frame_array = []
 files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
 #for sorting the file names properly
-files.sort(key = lambda x: x[5:-4])
-files.sort()
-frame_array = []
-files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+# files.sort(key = lambda x: x[5:-4])
+# files.sort()
+# frame_array = []
+# files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
 #for sorting the file names properly
-files.sort(key = lambda x: x[5:-4])
-for i in range(len(files)):
+# files.sort(key = lambda x: x[5:-4])
+i = 0;
+while (i < len(files)):
     filename=pathIn + files[i]
     #reading each files
-    img = cv.imread(filename)
+    img = cv.imread(str(filename))
+    # print(img)
     height, width, layers = img.shape
     size = (width,height)
-
     #inserting the frames into an image array
     frame_array.append(img)
+    i += 1
 out = cv.VideoWriter(pathOut,cv.VideoWriter_fourcc(*'DIVX'), fps, size)
 for i in range(len(frame_array)):
     # writing to a image array
